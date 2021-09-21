43bf476f0aefc8be5f210cf7f4dc52b92a09f61f
jemcmullin <jemcmullin@gmail.com>
2021-09-21 12:27:09 -0600

    add bloc states

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -3,11 +3,13 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../domain/entities/number_trivia.dart';
+
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial());
+  NumberTriviaBloc() : super(Empty());
 
   @override
   Stream<NumberTriviaState> mapEventToState(
