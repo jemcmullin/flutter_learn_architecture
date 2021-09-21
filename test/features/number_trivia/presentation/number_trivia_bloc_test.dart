f9ec8eac3d22a9bc895765f7b3b586fa707c6095
jemcmullin <jemcmullin@gmail.com>
2021-09-21 13:04:21 -0600

    test bloc initial state

--- test/features/number_trivia/presentation/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/number_trivia_bloc_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_learn_architecture/core/util/input_converter.dart';
 import 'package:flutter_learn_architecture/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:flutter_learn_architecture/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
@@ -10,12 +12,17 @@ import 'package:mockito/mockito.dart';
 
 @GenerateMocks([GetConcreteNumberTrivia, GetRandomNumberTrivia, InputConverter])
 void main() {
-  final getConcrete = MockGetConcreteNumberTrivia();
-  final getRandom = MockGetRandomNumberTrivia();
-  final inputConverter = MockInputConverter();
-  final numberTriviaBloc = NumberTriviaBloc(
-    concrete: getConcrete,
-    random: getRandom,
-    inputConverter: inputConverter,
+  final mockGetConcreteNumberTrivia = MockGetConcreteNumberTrivia();
+  final mockGetRandomNumberTrivia = MockGetRandomNumberTrivia();
+  final mockInputConverter = MockInputConverter();
+  final bloc = NumberTriviaBloc(
+    concrete: mockGetConcreteNumberTrivia,
+    random: mockGetRandomNumberTrivia,
+    inputConverter: mockInputConverter,
   );
+
+  test('bloc initialState should be empty', () async {
+    //assert
+    expect(bloc.state, Empty());
+  });
 }
