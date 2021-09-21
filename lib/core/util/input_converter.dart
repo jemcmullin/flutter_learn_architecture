c2b38075ed02397e736e4cf383a6746a22d0a220
jemcmullin <jemcmullin@gmail.com>
2021-09-21 11:01:26 -0600

    scaffolding bloc and input converter

---
+++ lib/core/util/input_converter.dart
@@ -0,0 +1,12 @@
+import 'package:dartz/dartz.dart';
+
+import '../error/failures.dart';
+
+class InputConverter {
+  Either<Failure, int> stringToUnsignedInteger(String str) {
+    //TODO
+    throw UnimplementedError();
+  }
+}
+
+class InvalidInputFailure extends Failure {}
