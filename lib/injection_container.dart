d0860a288ba0d4d4d4a487d2ae351b4dcebe056c
jemcmullin <jemcmullin@gmail.com>
2021-09-23 09:57:32 -0600

    fix imports and setup first page

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,17 +1,17 @@
-import 'package:flutter_learn_architecture/core/network/network_info.dart';
-import 'package:flutter_learn_architecture/core/util/input_converter.dart';
-import 'package:flutter_learn_architecture/features/number_trivia/data/repositories/number_trivia_repository_implementation.dart';
-import 'package:flutter_learn_architecture/features/number_trivia/data/sources/number_trivia_remote_source.dart';
-import 'package:flutter_learn_architecture/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
-import 'package:flutter_learn_architecture/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
-import 'package:flutter_learn_architecture/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
 
+import 'core/network/network_info.dart';
+import 'core/util/input_converter.dart';
+import 'features/number_trivia/data/repositories/number_trivia_repository_implementation.dart';
 import 'features/number_trivia/data/sources/number_trivia_local_source.dart';
+import 'features/number_trivia/data/sources/number_trivia_remote_source.dart';
 import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 
 //Service Locator
 final getIt = GetIt.instance;
